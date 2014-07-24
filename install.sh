282425c7d6353202583155623bf64d61f2b2017d
Zhang Jingwang <jingwang.zhang@emc.com>
2014-07-24 10:23:26 +0800

    Add github links to all the required libraries
    And add a bash script to install them to ~/.emacs.d/. This should
    work with Emacs 24.3.

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p ~/.emacs.d
+# Install auto-complete
+# Fix up github urls.
+sed -i s/git:/https:/g ./lib/auto-complete/.gitmodules
+(cd ./lib/auto-complete; git submodule init ; git submodule update)
+make -C ./lib/auto-complete install DIR=~/.emacs.d/
+# auto-complete depends on popup.el
+cp -f ./lib/auto-complete/lib/popup/popup.el ~/.emacs.d/
+# Install xcscope
+cp -f ./lib/xcscope.el/xcscope.el ~/.emacs.d/
+cp -f ./lib/yasnippet/yasnippet.el ~/.emacs.d/
+cp .emacs ~/.emacs
